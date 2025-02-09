0de44400fc55693a5b2c81800db17f794f59a236
Bobea Dorin-Adrian <bobeadorin@yahoo.com>
2025-02-09 14:14:33 +0200

    Project innit

---
+++ AuthService/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] 
+    public class AuthController : ControllerBase
+    {
+
+        public AuthController()
+        {
+            
+        }
+
+        [HttpGet("/")]
+        public IActionResult Get() { 
+        
+            return Ok("it works");
+        }
+
+    }
+}
